87af9fe89640ebac77a7f52707b5a74f97ecc7e6
irudite <KerrickTruong@gmail.com>
2025-01-02 03:31:46 -0500

    Finished styling create-trip page

--- src/pages/create-trip/Trip.jsx
+++ src/pages/create-trip/Trip.jsx
@@ -14,14 +14,17 @@ const Trip = () => {
       </div>
 
       <div class='trip__input'>
+        <h2> Please list your destination </h2>
         <input placeholder='Enter a place'/> 
-        <input type='number' placeholder='How many days will you be staying?'/> 
+        <h2> How many days will you be staying? </h2>
+        <input type='number' placeholder='i.g 2 days'/> 
       </div>
 
       <div class='trip__options'>
+        <h2> What is your budget? </h2>
         <div class='trip__options--cost'>
           {budgetSize.map((item, index) => (
-            <div key={index}>
+            <div class='trip__options--cost--choices' key={index}>
               <h2>{item.icon}</h2>
               <h2>{item.title}</h2>
               <h2>{item.desc}</h2>
@@ -29,9 +32,10 @@ const Trip = () => {
           ))} 
         </div>
 
+        <h2> How many people are you traveling with? </h2>
         <div class='trip__options--size'>
           {travelSize.map((item, index) => (
-            <div key={index}>
+            <div class='trip__options--size--choices' key={index}>
               <h2>{item.icon} ({item.people} People)</h2>
               <h2>{item.title}</h2>
               <h2>{item.desc}</h2>

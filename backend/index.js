352e4e15ec53bc43f9a02d797f85f4cfe4adc4f1
irudite <KerrickTruong@gmail.com>
2025-01-06 17:18:36 -0500

    Setting up more backend file structure

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json())
+app.use(bodyParser.urlencoded( {extended: true} );
 
 //routes
-app.get('/', (req, res) => {
-  res.send('It worked!')
-});
+const tripRoutes = require('./routes/tripRoutes.js');
+app.use('')
 
 //Start server
 app.listen(PORT, () => {
   console.log('Server running on port: ' + PORT);
-})
+});

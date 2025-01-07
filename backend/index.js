a8c9dfed12fb91cbb519d762133bb7bf91db7236
irudite <KerrickTruong@gmail.com>
2025-01-06 21:08:11 -0500

    Reprogrammed server

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-//middleware
-app.use(cors());
-app.use(express.json())
-app.use(bodyParser.urlencoded( {extended: true} );
+app.use(express.json());
 
-//routes
 const tripRoutes = require('./routes/tripRoutes.js');
-app.use('')
+app.use('/api/trips', tripRoutes);
 
-//Start server
-app.listen(PORT, () => {
-  console.log('Server running on port: ' + PORT);
-});
+app.listen(5000, () => {
+  console.log("Server is connected to http://localhost:5000")
+})
